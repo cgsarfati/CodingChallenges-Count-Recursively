bcacfcdf07b318a400886345e644ba0ec9776ddb
vagrant <vagrant@vagrant.vm>
2017-09-25 22:14:08 +0000

    uploaded demo file

---
+++ recursion.py
@@ -0,0 +1,146 @@
+"""Recursion lecture examples."""
+
+
+def count_loop():
+    """Count to 3, using a while loop."""
+    n = 1
+    while True:
+        if n > 3:
+            break
+
+        print n,
+        n = n + 1
+
+
+print "count_loop"
+count_loop()
+
+
+def count_recursive(n=1):
+    """Count to 3, using recursion."""
+    if n > 3:
+        return
+    print n,
+    count_recursive(n + 1)
+    print n,
+
+
+print "\n\ncount_recursive"
+count_recursive()
+
+###########################################################
+
+data = [1, [2, [3], 4], 5]  # => 2 4 6 8 10
+
+
+def doubler_no_recursion(lst):
+    """Print items in list doubled, not using recursion."""
+
+    stack = list(reversed(lst))
+
+    while stack:
+        n = stack.pop()
+        if isinstance(n, list):
+            # If it's a list, add it to stack, reversed
+            for inner in reversed(n):
+                stack.append(inner)
+        else:
+            print n * 2,
+
+
+print "\n\ndoubler_no_recursion"
+doubler_no_recursion(data)
+
+
+def doubler_recursive(lst):
+    """Print items in list doubled, using recursion."""
+
+    for n in lst:
+        if isinstance(n, list):
+            doubler_recursive(n)
+        else:
+            print n * 2,
+
+
+print "\n\ndoubler_recursive"
+doubler_recursive(data)
+
+
+def doubler_no_loop(lst):
+    """Print items in list doubled, using recursion but no loop."""
+
+    if lst == []:
+        return
+
+    n = lst[0]
+
+    if isinstance(n, list):
+        doubler_no_loop(n)
+    else:
+        print n * 2,
+
+    doubler_no_loop(lst[1:])
+
+
+print "\n\ndoubler_no_loop"
+doubler_no_loop(data)
+
+###########################################################
+
+lst = [7, 4, 3, 2]
+
+
+def lenlist(lst):
+    """Return length of list, using recursion."""
+
+    if not lst:
+        return 0
+
+    return 1 + lenlist(lst[1:])
+
+
+print "\n\nlenlist"
+print lenlist(lst)
+
+
+###########################################################
+
+
+class Node(object):
+    """Tree node."""
+
+    def __init__(self, data, children=None):
+        self.data = data
+        self.children = children or []
+
+
+root = Node("/", [
+    Node("Users/", [
+        Node("jane/", [Node("resume.txt"), Node("recipes.txt")]),
+        Node("jessica/", [Node("server.py")])
+    ])
+])
+
+
+def ls(node):
+    """List all nodes."""
+
+    print node.data
+    for child in node.children:
+        ls(child)
+
+
+print "\n\nls\n"
+ls(root)
+
+
+def indent_ls(node, depth=0):
+    """List all nodes, indented."""
+
+    print "  " * depth + node.data
+    for child in node.children:
+        indent_ls(child, depth + 1)
+
+
+print "\n\nindent_ls\n"
+indent_ls(root)
